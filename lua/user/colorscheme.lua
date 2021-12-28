5df6956941047ca002c8c6eeff475529defe05f7
Luka Krapic <luka.krapic@gmail.com>
2021-12-28 17:30:31 +0100

    Add colorscheme configuration for everforrest
    Add missing ocnfiguration for everforrest colorscheme.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,11 @@
 local colorscheme = "everforest"
 
+-- everforrest colorscheme configuration
+if colorscheme == "everforest" then
+  vim.g.everforest_enable_italic = 1
+  vim.g.everforest_better_performance = 1
+end
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("Colorscheme " .. colorscheme .. " not found!")
