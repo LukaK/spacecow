23fe3980832b4eebfb6b062960cd0853abed56c6
Luka Krapic <luka.krapic@gmail.com>
2021-12-28 17:58:43 +0100

    Improve comments in init.lua
    Improving code quality.

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
+-- source lua config module
 require "user"
 
--- source vimscript files that are not yet supported
+-- source vimscript configuration
 local nvim_path = vim.fn.stdpath('config')
 local vimscript_files = {"autocommands.vim"}
 
-for _, vimscript_file in pairs(vimscript_files) do
+for _, vimscript_file in ipairs(vimscript_files) do
   vim.cmd("source " .. nvim_path .. "/vimscript/" .. vimscript_file)
 end
