e74b612a714ec229139ffff915a65f5760f3815e
Luka Krapic <luka.krapic@gmail.com>
2021-12-28 17:50:51 +0100

    Refactor globals lua config file
    Improve code quality.
    
    Move helper function to utils. Initialize global variables using for
    loop.

--- lua/user/globals.lua
+++ lua/user/globals.lua
@@ -1,53 +1,59 @@
 -- local function file_exists(name)
-function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then io.close(f) return true else return false end
+local utils = require "user.utils"
+
+-- set python 3 path
+local python3_path = vim.fn.exepath("python")
+local python3_pyenv_path = vim.env["PYENV_ROOT"] .. '/versions/py3nvim/python'
+
+if utils.file_exists(python3_pyenv_path) then
+  python3_path = python3_pyenv_path
 end
 
--- TODO: Add default provider if pyenv did not provided one
--- set python3 provider
-local pyenv_root = vim.env["PYENV_ROOT"]
-local python3_path = pyenv_root .. '/versions/py3nvim/python'
+local glob_options = {
+  -- python 3 provider
+  python3_host_prog = python3_path,
 
-if file_exists(python3_path) then
-  vim.g.python3_host_prog = python3_path
+  -- disable python2 provider
+  loaded_python_provider = 0,
+
+  -- disable perl provider
+  loaded_perl_provider = 0,
+
+  -- disable ruby provider
+  loaded_ruby_provider = 0,
+
+  -- disable node provider
+  loaded_node_provider = 0,
+
+  -- leader key
+  mapleader = ",",
+  maplocalleader = ",",
+
+  -- enable highlighting for lua HERE doc inside vim script
+  vimsyn_embed = 'l',
+
+  -- use English as main language
+  language = "en_US.utf-8",
+
+  -- do not load tohtml.vim
+  loaded_2html_plugin = 1,
+
+  -- do not load zipPlugin.vim, gzip.vim and tarPlugin.vim (all these plugins are
+  -- related to checking files inside compressed files)
+  loaded_zipPlugin = 1,
+  loaded_tarPlugin = 1,
+  loaded_gzip = 1,
+
+  -- do not load tutor plugin
+  loaded_tutor_mode_plugin = 1,
+
+  -- do not use builtin matchit.vim and matchparen.vim since we use vim-matchup
+  loaded_matchit = 1,
+  loaded_matchparen = 1,
+
+}
+
+-- set global vim variables
+for key,val in pairs(glob_options) do
+  vim.g[key] = val
 end
-
-
--- disable python2 provider
-vim.g.loaded_python_provider = 0
-
--- disable perl provider
-vim.g.loaded_perl_provider = 0
-
--- disable ruby provider
-vim.g.loaded_ruby_provider = 0
-
--- disable node provider
-vim.g.loaded_node_provider = 0
-
--- leader key
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
-
--- enable highlighting for lua HERE doc inside vim script
-vim.g.vimsyn_embed = 'l'
-
--- use English as main language
-vim.g.language = "en_US.utf-8"
-
--- do not load tohtml.vim
-vim.g.loaded_2html_plugin = 1
-
--- do not load zipPlugin.vim, gzip.vim and tarPlugin.vim (all these plugins are
--- related to checking files inside compressed files)
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_gzip = 1
-
--- do not load tutor plugin
-vim.g.loaded_tutor_mode_plugin = 1
-
--- do not use builtin matchit.vim and matchparen.vim since we use vim-matchup
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
