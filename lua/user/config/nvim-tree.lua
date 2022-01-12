8d1875ce92ac8e351f34013c975aad70aa6c5c1d
Luka Krapic <luka.krapic@gmail.com>
2022-01-12 18:56:40 +0100

    Add filter for windows in nvim-tree
    Bug fix, when vista is opened, opening new file from nvim-tree asks for
    window number.
    
    Add filetype exclusion to nvim-tree for vista window.

--- lua/user/config/nvim-tree.lua
+++ lua/user/config/nvim-tree.lua
@@ -6,10 +6,15 @@ end
 nvim_tree.setup {
   auto_close = true,
   diagnostics = {
-    enable = true,
+    enable = false,
   },
   view = {
     number = true,
     relativenumber = true,
   }
+}
+
+vim.g["nvim_tree_window_picker_exclude"] = {
+  filetype = {"notify", "packer", "qf", "vista"},
+  buftype = {"terminal"}
 }
