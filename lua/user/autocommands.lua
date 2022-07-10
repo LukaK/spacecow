88cd191a83ad24b80c8f2f6d19bf46b833af3b89
Luka Krapic <luka.krapic@gmail.com>
2022-07-10 16:07:27 +0200

    Rewrite part of autocommands vim file to lua
    Refactoring moving from vimscript to lua.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -8,3 +8,77 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end
 })
+
+-- more accurate syntax highlighting
+vim.api.nvim_create_autocmd("BufEnter", { nested = true, callback = function() vim.api.nvim_command("syntax sync fromstart") end })
+
+-- relative number toggle on focus
+vim.api.nvim_create_autocmd({"BufEnter", "FocusGained", "InsertLeave", "WinEnter"}, { nested = true, callback = function() if (vim.opt_local.number) then vim.opt_local.relativenumber = true end end })
+vim.api.nvim_create_autocmd({"BufLeave", "FocusLost", "InsertEnter", "WinLeave"}, { nested = true, callback = function() if (vim.opt_local.number) then vim.opt_local.relativenumber = false end end })
+
+-- do not use smart case in command line mode, extracted from https://vi.stackexchange.com/a/16511/15292.
+vim.api.nvim_create_autocmd("CmdLineEnter", { nested = true, callback = function() vim.api.nvim_set_option("smartcase", false) end })
+vim.api.nvim_create_autocmd("CmdLineLeave", { nested = true, callback = function() vim.api.nvim_set_option("smartcase", true) end })
+
+-- terminal settings
+vim.api.nvim_create_autocmd("TermOpen", { nested = true, callback = function() vim.api.nvim_command("startinsert") end })
+vim.api.nvim_create_autocmd("TermOpen", {
+  nested = true,
+  callback = function()
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+  end
+})
+
+-- TODO: Fix this so that you use nvim notify plugin ( plugins should be loaded first? )
+-- display a message when the current file is not in utf-8 format.
+vim.api.nvim_create_autocmd("BufRead", {
+  nested = true,
+  callback = function()
+    if (vim.api.nvim_buf_get_option(0, "fileencoding") ~= "utf-8")
+    then
+      vim.notify("File not in UTF-8 format!", "warn", {title = "nvim-config"})
+    end
+  end
+})
+
+
+-- automatically reload the file if it is changed outside of Nvim, see
+-- https://unix.stackexchange.com/a/383044/221410. It seems that `checktime`
+-- command does not work in command line. We need to check if we are in command
+-- line before executing this command. See also
+-- https://vi.stackexchange.com/a/20397/15292.
+vim.api.nvim_create_autocmd("FileChangedShellPost", { nested = true, callback = function() vim.notify("File changed on disk. Buffer reloaded!", 'warn', {title = 'nvim-config'}) end })
+-- TODO: fix this
+-- vim.api.nvim_create_autocmd({"FocusGained", "CursorHold"}, {
+--   nested = true,
+--   callback = function()
+--     if (vim.api.nvim_exec("getcmdwintype", true) ==  "") then vim.api.nvim_command("checktime") end
+--   end
+-- })
+
+
+-- TODO: rewrite this
+-- return to last edit position when opening a file
+-- vim.api.nvim_create_autocmd("BufEnter", { nested = true, callback = function() vim.api.nvim_command("syntax sync fromstart") end })
+-- " Only resume last edit position when there is no go-to-line command (something like '+23').
+-- function s:resume_edit_pos() abort
+--   if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit'
+--     let l:args = v:argv  " command line arguments
+--     for l:cur_arg in l:args
+--       " Check if a go-to-line command is given.
+--       let idx = match(l:cur_arg, '\v^\+(\d){1,}$')
+--       if idx != -1
+--         return
+--       endif
+--     endfor
+--
+--     execute "normal! g`\"zvzz"
+--   endif
+-- endfunction
+--
+-- " Return to last edit position when opening a file
+-- augroup resume_edit_position
+--   autocmd!
+--   autocmd BufReadPost * call s:resume_edit_pos()
+-- augroup END
--- vimscript/autocommands.vim
+++ vimscript/autocommands.vim
@@ -1,25 +1,4 @@
-" TODO: Rewrite to lua
-" Do r r not use smart case in command line mode, extracted from https://vi.stackexchange.com/a/16511/15292.
-augroup dynamic_smartcase
-  autocmd!
-  autocmd CmdLineEnter : set nosmartcase
-  autocmd CmdLineLeave : set smartcase
-augroup END
-
-augroup term_settings
-  autocmd!
-  " Do not use number and relative number for terminal inside nvim
-  autocmd TermOpen * setlocal norelativenumber nonumber
-  " Go to insert mode by default to start typing command
-  autocmd TermOpen * startinsert
-augroup END
-
-" More accurate syntax highlighting? (see `:h syn-sync`)
-augroup accurate_syn_highlight
-  autocmd!
-  autocmd BufEnter * :syntax sync fromstart
-augroup END
-
+" TODO: Rewrite this to lua
 " Return to last edit position when opening a file
 augroup resume_edit_position
   autocmd!
@@ -42,28 +21,14 @@ function s:resume_edit_pos() abort
   endif
 endfunction
 
-" Display a message when the current file is not in utf-8 format.
-" Note that we need to use `unsilent` command here because of this issue:
-" https://github.com/vim/vim/issues/4379
-augroup non_utf8_file_warn
-  autocmd!
-  " we can not use `lua vim.notify()`: it will error out E5107 parsing lua.
-  autocmd BufRead * if &fileencoding != 'utf-8' | call v:lua.vim.notify('File not in UTF-8 format!', 'warn', {'title': 'nvim-config'}) | endif
-augroup END
-
-" Automatically reload the file if it is changed outside of Nvim, see
-" https://unix.stackexchange.com/a/383044/221410. It seems that `checktime`
-" command does not work in command line. We need to check if we are in command
-" line before executing this command. See also
-" https://vi.stackexchange.com/a/20397/15292.
-augroup auto_read
-  autocmd!
-  autocmd FileChangedShellPost * call v:lua.vim.notify("File changed on disk. Buffer reloaded!", 'warn', {'title': 'nvim-config'})
-  autocmd FocusGained,CursorHold * if getcmdwintype() == '' | checktime | endif
-augroup END
-
-augroup numbertoggle
-  autocmd!
-  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu | set rnu   | endif
-  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu | set nornu | endif
-augroup END
+" TODO: Rewrite this fully in lua file
+" " Automatically reload the file if it is changed outside of Nvim, see
+" " https://unix.stackexchange.com/a/383044/221410. It seems that `checktime`
+" " command does not work in command line. We need to check if we are in command
+" " line before executing this command. See also
+" " https://vi.stackexchange.com/a/20397/15292.
+" augroup auto_read
+"   autocmd!
+"   autocmd FileChangedShellPost * call v:lua.vim.notify("File changed on disk. Buffer reloaded!", 'warn', {'title': 'nvim-config'})
+"   autocmd FocusGained,CursorHold * if getcmdwintype() == '' | checktime | endif
+" augroup END
