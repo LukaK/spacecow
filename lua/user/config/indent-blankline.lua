22cc2468d2b50434a40c5b29e1e984f8b99c5939
Luka Krapic <luka.krapic@gmail.com>
2023-10-13 13:41:19 +0200

    Update blankline configuration to version 3.

--- lua/user/config/indent-blankline.lua
+++ lua/user/config/indent-blankline.lua
@@ -1,11 +1,19 @@
 require('ibl').setup({
   -- U+2502 may also be a good choice, it will be on the middle of cursor.
   -- U+250A is also a good choice
-  char = "▏",
-  show_end_of_line = true,
-  disable_with_nolist = true,
-  buftype_exclude = { "terminal" },
-  filetype_exclude = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha" },
+  indent = {
+    char = "▏"
+  },
+  scope = {
+    show_end = true
+  },
+  exclude = {
+    buftypes = {"terminal"},
+    filetypes = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha" },
+  }
+  -- disable_with_nolist = true,
+  -- buftype_exclude = { "terminal" },
+  -- filetype_exclude = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha" },
 })
 
 -- check if indentation line impact prductivity when present in insert mode
